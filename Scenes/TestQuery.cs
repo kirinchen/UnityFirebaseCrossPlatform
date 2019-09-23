eb25207cf5b3459e43a94b486e19a00085aa738e
kirin <kirin.chen1001@gmail.com>
2019-09-23 14:49:31 +0800

    add Query

--- Scenes/TestQuery.cs
+++ Scenes/TestQuery.cs
@@ -1,5 +1,7 @@
 ﻿using Firebase.Database;
+using surfm.tool;
 using surfm.tool.realtimedb;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TestQuery : MonoBehaviour {
@@ -19,19 +21,41 @@ public class TestQuery : MonoBehaviour {
         Debug.Log("onTestQuery!");
 
         DatabaseReference f = FirebaseDatabase.DefaultInstance.RootReference.Child("numbers");
-        Query qf = f.OrderByChild("value").StartAt(3);
+        Query qf = f.OrderByChild("x").StartAt(30);
+        qf = qf.OrderByChild("y").StartAt(30);
         qf.ChildAdded += (s, e) => {
             string json = e.Snapshot.GetRawJsonValue();
             Debug.Log("ChildAdded json:" + json);
         };
-        qf.ValueChanged += (s, e) => {
+        /*qf.ValueChanged += (s, e) => {
             string json = e.Snapshot.GetRawJsonValue();
             Debug.Log("ValueChanged json:" + json);
-        };
+        }; */
         //f.OrderByChild("name").EqualTo("one").ChildAdded += (s, e) => {
         //    string json = e.Snapshot.GetRawJsonValue();
         //    Debug.Log("json:" + json);
         //};
+    }
+
+    public class TestData {
+        public float x;
+        public float y;
+        public string name;
+    }
+
+    public void putTestData() {
+        DatabaseReference f = FirebaseDatabase.DefaultInstance.RootReference.Child("numbers");
+        List<TestData> list = new List<TestData>();
+        for (int i=0;i<20;i++) {
+            list.Add(new TestData() {
+                x = Random.Range(1, 100),
+                y = Random.Range(1, 100),
+                name = "test_"+i
+            });
+        }
+        f.SetRawJsonValueAsync(CommUtils.toJson(list) );
+
+
     }
 
 
