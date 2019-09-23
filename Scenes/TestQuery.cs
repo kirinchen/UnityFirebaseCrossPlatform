3eb36b8fd838dc621e4a2930da53de3517a231e4
kirin <kirin.chen1001@gmail.com>
2019-09-23 23:18:27 +0800

    Add GeoHashUtils.cs

--- Scenes/TestQuery.cs
+++ Scenes/TestQuery.cs
@@ -11,36 +11,39 @@ public class TestQuery : MonoBehaviour {
     void Start() {
         RealtimeDBFactory.get().initDoneCB().add(i => {
             realtimeDB = i;
-            realtimeDB.subscribe("", s => { Debug.Log("subscribe:" + s); });
-            onTestQuery();
+            realtimeDB.subscribe("geo", s => { onReslut(s); });
+          
         });
     }
 
 
-    public void onTestQuery() {
-        Debug.Log("onTestQuery!");
+    private void onReslut(string s) {
+        List<TestData> list = CommUtils.convertByJson<List<TestData>>(s);
+        TestData t11 = list.Find(d=> d.x == 1 && d.y==1);
+        TestData t55 = list.Find(d => d.x == 5 && d.y == 5);
+        if (t11 == null) return;
+        if (t55 == null) return;
+        Debug.Log("Go Query");
+        onTestQuery(t11.hash,t55.hash);
+    }
 
-        DatabaseReference f = FirebaseDatabase.DefaultInstance.RootReference.Child("numbers");
-        Query qf = f.OrderByChild("x").StartAt(30);
-        qf = qf.OrderByChild("y").StartAt(30);
+    public void onTestQuery(string st,string end) {
+        Debug.Log("onTestQuery! st:"+ st+" end:"+ end);
+
+        DatabaseReference f = FirebaseDatabase.DefaultInstance.RootReference.Child("geo");
+        //1 1  s00twy                4 4   s0dyg0
+        Query qf = f.OrderByChild("hash").StartAt(st).EndAt(end);
         qf.ChildAdded += (s, e) => {
             string json = e.Snapshot.GetRawJsonValue();
             Debug.Log("ChildAdded json:" + json);
         };
-        /*qf.ValueChanged += (s, e) => {
-            string json = e.Snapshot.GetRawJsonValue();
-            Debug.Log("ValueChanged json:" + json);
-        }; */
-        //f.OrderByChild("name").EqualTo("one").ChildAdded += (s, e) => {
-        //    string json = e.Snapshot.GetRawJsonValue();
-        //    Debug.Log("json:" + json);
-        //};
     }
 
     public class TestData {
         public float x;
         public float y;
         public string name;
+        public string hash;
     }
 
     public void putTestData() {
@@ -54,8 +57,25 @@ public class TestQuery : MonoBehaviour {
             });
         }
         f.SetRawJsonValueAsync(CommUtils.toJson(list) );
+    }
 
-
+    public void testGeoHash() {
+        DatabaseReference f = FirebaseDatabase.DefaultInstance.RootReference.Child("geo");
+        Map<string, Vector2Int> map = new Map<string, Vector2Int>();
+        List<TestData> list = new List<TestData>();
+        for (int i=-10;i<10;i++) {
+            for (int j = -10; j < 10; j++) {
+                Vector2Int v = new Vector2Int(i,j);
+                string hash = GeoHashUtils.calcGeoHash(i,j,12);
+                list.Add(new TestData() {
+                    x =i,
+                    y=j,
+                    name= "GEO_"+i+"_"+j,
+                    hash = hash
+                });
+            }
+        }
+        f.SetRawJsonValueAsync(CommUtils.toJson(list));
     }
 
 
---
+++ Utils/GeoHashUtils.cs
@@ -0,0 +1,58 @@
+﻿namespace surfm.tool.realtimedb {
+    public class GeoHashUtils {
+
+        private static readonly int BITS_PER_BASE32_CHAR = 5;
+        private static readonly int MAX_PRECISION = 22;
+        public static readonly string BASE32_CHARS = "0123456789bcdefghjkmnpqrstuvwxyz";
+        private static string bucketUrl = ConstantRepo.getInstance().get<string>("Firebase.Storage.bucketUrl");
+
+        private readonly string geoHash;
+
+        public static string calcGeoHash(double latitude, double longitude, int precision) {
+            if (precision < 1) {
+                throw new System.Exception("Precision of GeoHash must be larger than zero!");
+            }
+            if (precision > MAX_PRECISION) {
+                throw new System.Exception("Precision of a GeoHash must be less than " + (MAX_PRECISION + 1) + "!");
+            }
+            if (!coordinatesValid(latitude, longitude)) {
+                throw new System.Exception(string.Format("Not valid location coordinates: [{0}, {1}]", latitude, longitude));
+            }
+            double[] longitudeRange = { -180, 180 };
+            double[] latitudeRange = { -90, 90 };
+
+            char[] buffer = new char[precision];
+
+            for (int i = 0; i < precision; i++) {
+                int hashValue = 0;
+                for (int j = 0; j < BITS_PER_BASE32_CHAR; j++) {
+                    bool even = (((i * BITS_PER_BASE32_CHAR) + j) % 2) == 0;
+                    double val = even ? longitude : latitude;
+                    double[] range = even ? longitudeRange : latitudeRange;
+                    double mid = (range[0] + range[1]) / 2;
+                    if (val > mid) {
+                        hashValue = (hashValue << 1) + 1;
+                        range[0] = mid;
+                    } else {
+                        hashValue = (hashValue << 1);
+                        range[1] = mid;
+                    }
+                }
+                buffer[i] = valueToBase32Char(hashValue);
+            }
+            return new string(buffer);
+        }
+
+        public static bool coordinatesValid(double latitude, double longitude) {
+            return (latitude >= -90 && latitude <= 90 && longitude >= -180 && longitude <= 180);
+        }
+
+        public static char valueToBase32Char(int value) {
+            if (value < 0 || value >= BASE32_CHARS.Length) {
+                throw new System.Exception("Not a valid base32 value: " + value);
+            }
+            return BASE32_CHARS[(value)];
+        }
+
+    }
+}
